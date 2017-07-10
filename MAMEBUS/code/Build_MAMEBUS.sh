481a315c7b42d5841550f1e42739ad9c904fe1eb
Andrew Stewart <Andrew@aos-213.atmos.ucla.edu>
2017-07-10 15:33:01 -0700

    More restructuring

--- MAMEBUS/code/Build_MAMEBUS.sh
+++
@@ -1,3 +0,0 @@
-gcc -O3 -ffast-math -c rktvd.c defs.c
-gcc -O3 -ffast-math -lm mamebus.c -o mamebus.exe -Xlinker rktvd.o defs.o
-rm *.o
--- MAMEBUS/code/Build_MAMEBUS_intel.sh
+++
@@ -1,4 +0,0 @@
-source /opt/intel/bin/compilervars.sh intel64
-icc -parallel -par-report3 -O3 -c rktvd.c defs.c
-icc -parallel -par-report3 -O3 mamebus.c -o mamebus.exe -Xlinker rktvd.o defs.o
-rm *.o
